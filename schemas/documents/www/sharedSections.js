73eb58c084e0e4329b3e018efa45251ff5274f02
David Daniel <davedan7@gmail.com>
2022-03-01 15:29:54 -0700

    Add description to shared sections

--- schemas/documents/www/sharedSections.js
+++ schemas/documents/www/sharedSections.js
@@ -7,12 +7,16 @@ export default {
     {
       title: 'Newsletter Section',
       name: 'newsletter',
+      description:
+        'Shared newsletter signup section - currently used in the /science and /token pages',
       type: 'titleCustomBody',
       validation: Rule => Rule.required(),
     },
     {
       title: 'Blog Section',
       name: 'blog',
+      description:
+        'Shared blog section - currently used in the marketing home page and /science page',
       type: 'blogSection',
       validation: Rule => Rule.required(),
     },
