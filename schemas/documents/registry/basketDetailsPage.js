c872be593bc9b136a14e88d4014147ec9aab556d
Florent <florent.lagrede@gmail.com>
2023-01-11 10:18:41 +0100

    doc: update readme about documents actions

--- schemas/documents/registry/basketDetailsPage.js
+++ schemas/documents/registry/basketDetailsPage.js
@@ -2,7 +2,7 @@ export default {
   name: 'basketDetailsPage',
   type: 'document',
   title: 'Basket Details Page',
-  __experimental_actions: ['update', /*'create', 'delete', */ 'publish'],
+  __experimental_actions: ['update', /* 'create', 'delete', */ 'publish'],
   fields: [
     {
       name: 'gettingStartedResourcesCard',
