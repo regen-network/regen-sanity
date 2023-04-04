746f2695b86565b9c2f0e8e5312dbe5e84ec8199
Florent <florent.lagrede@gmail.com>
2023-04-04 16:16:33 +0200

    feat: add sdgs to ecologicalImpact

--- schemas/documents/shared/ecologicalImpact.js
+++ schemas/documents/shared/ecologicalImpact.js
@@ -37,6 +37,18 @@ export default {
       validation: Rule => Rule.required(),
     },
     {
+      title: 'SDGs',
+      name: 'sdgs',
+      type: 'array',
+      description: 'Select existing SDGs from "Shared" content',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'sdg' }],
+        },
+      ],
+    },
+    {
       title: 'Standard',
       name: 'standard',
       type: 'customImage',
