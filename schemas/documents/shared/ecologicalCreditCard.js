c7c2659870aa998746c20d9ca633fc266f069352
Florent Lagrede <florent.lagrede@gmail.com>
2023-12-07 13:37:44 +0100

    feat: buyers page optional data (#52)
    * feat: make offsetMethods in ecologicalCreditCard optional
    
    * feat: add secondary button in ecologicalCreditCard

--- schemas/documents/shared/ecologicalCreditCard.js
+++ schemas/documents/shared/ecologicalCreditCard.js
@@ -50,7 +50,6 @@ export default {
           to: [{ type: 'offsetMethod' }],
         },
       ],
-      validation: Rule => Rule.required().min(1),
     },
     {
       title: 'Project Activities',
@@ -66,6 +65,11 @@ export default {
     {
       title: 'Button',
       name: 'button',
+      type: 'button',
+    },
+    {
+      title: 'Secondary Button',
+      name: 'secondaryButton',
       type: 'button',
     },
   ],
